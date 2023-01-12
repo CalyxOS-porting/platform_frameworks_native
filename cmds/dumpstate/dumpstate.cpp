6db226d98ef2ec535e85d656054564725b19159b
Tommy Webb <tommy@calyxinstitute.org>
2023-01-12 09:38:44 -0500

    Make bug report vibration more noticeable
    * Increase vibration time when starting a bug report.
    * Increase vibration time and frequency when ending a bug report.
    
    Change-Id: I903f18f8c187681591acd283a321a71cc515eb4a

--- cmds/dumpstate/dumpstate.cpp
+++ cmds/dumpstate/dumpstate.cpp
@@ -2982,7 +2982,7 @@ Dumpstate::RunStatus Dumpstate::RunInternal(int32_t calling_uid,
     }
 
     if (options_->do_vibrate) {
-        Vibrate(150);
+        Vibrate(300);
     }
 
     if (zip_file != nullptr) {
@@ -3108,8 +3108,8 @@ Dumpstate::RunStatus Dumpstate::RunInternal(int32_t calling_uid,
 
     /* vibrate a few but shortly times to let user know it's finished */
     if (options_->do_vibrate) {
-        for (int i = 0; i < 3; i++) {
-            Vibrate(75);
+        for (int i = 0; i < 5; i++) {
+            Vibrate(125);
             usleep((75 + 50) * 1000);
         }
     }
